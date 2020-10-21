d896e155133362385ff7fbddd691586299125c10
Nguyen Duy Nguyen <nguyennd@greenglobal.vn>
2020-10-21 15:48:52 +0700

    ss

--- git-push.sh
+++ git-push.sh
@@ -6,6 +6,7 @@
 
 COMMIT_TIMESTAMP=`date +'%Y-%m-%d %H:%M:%S %Z'`
 
+git status
 git add .
 git commit -m "Automated commit on ${COMMIT_TIMESTAMP}"
 git push https://keeloren:Duynguyen1996@github.com/keeloren/file-management.git
