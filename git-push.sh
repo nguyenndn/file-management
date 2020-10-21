fb8475d69060f93daf143fb17f407a5249066932
Nguyen Duy Nguyen <nguyennd@greenglobal.vn>
2020-10-21 09:36:39 +0700

    Automated commit on 2020-10-21 09:36:39 +07

--- git-push.sh
+++ git-push.sh
@@ -1,5 +1,11 @@
-echo "Enter your message"
-read message
+#echo "Enter your message"
+#read message
+#git add .
+#git commit -m"${message}"
+#git push -u origin develop
+
+COMMIT_TIMESTAMP=`date +'%Y-%m-%d %H:%M:%S %Z'`
+
 git add .
-git commit -m"${message}"
-git push -u origin develop
+git commit -m "Automated commit on ${COMMIT_TIMESTAMP}"
+git push https://keeloren:Duynguyen1996@github.com/keeloren/file-management.git
