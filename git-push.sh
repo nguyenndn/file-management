dca4061760ce8b41261bd2954a5f2701fff9bafb
Nguyen Duy Nguyen <nguyennd@greenglobal.vn>
2020-10-21 09:34:58 +0700

    commit test

--- git-push.sh
+++ git-push.sh
@@ -1,5 +1,12 @@
-COMMIT_TIMESTAMP=`date +'%Y-%m-%d %H:%M:%S %Z'`
-
+echo "Enter your message"
+read message
 git add .
-git commit -m "Automated commit on ${COMMIT_TIMESTAMP}"
-git push https://keeloren:Duynguyen1996@github.com/keeloren/file-management.git
+git commit -m"${message}"
+if [ -n "$(git status - porcelain)" ];
+then
+ echo "IT IS CLEAN"
+else
+ git status
+ echo "Pushing data to remote server!!!"
+ git push -u origin master
+fi
