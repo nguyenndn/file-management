3bc171db7b6588ee2a2e21e7c515e29be11d8ffd
Nguyen Duy Nguyen <nguyennd@greenglobal.vn>
2020-10-21 09:35:40 +0700

    abc

--- git-push.sh
+++ git-push.sh
@@ -2,11 +2,4 @@ echo "Enter your message"
 read message
 git add .
 git commit -m"${message}"
-if [ -n "$(git status - porcelain)" ];
-then
- echo "IT IS CLEAN"
-else
- git status
- echo "Pushing data to remote server!!!"
- git push -u origin master
-fi
+git push -u origin develop
