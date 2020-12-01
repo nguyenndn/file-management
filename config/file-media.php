135a156507c8e9468e21be3d700675e37cab82ef
Nguyen Duy Nguyen <nguyennd@greenglobal.vn>
2020-12-01 10:08:48 +0700

    Automated commit on 2020-12-01 10:08:48 +07

--- config/file-media.php
+++ config/file-media.php
@@ -2,48 +2,55 @@
 
 $listDisks = ['local', 's3', 'gcs', 'minio'];
 
-//$disksConfig = [
-//    'local' => [
-//        'driver' => 'local',
-//        'root' => storage_path('app/public'),
-//        'url' => env('APP_URL') . '/storage/public',
-//        'visibility' => 'public',
-//    ],
-//    's3' => [
-//        'driver' => 's3',
-//        'key' => env('AWS_ACCESS_KEY_ID'),
-//        'secret' => env('AWS_SECRET_ACCESS_KEY'),
-//        'region' => env('AWS_DEFAULT_REGION'),
-//        'bucket' => env('AWS_BUCKET'),
-//        'url' => env('AWS_URL'),
-//    ],
-//    'gcs' => [
-//        'driver' => 'gcs',
-//        'project_id' => env('GOOGLE_CLOUD_PROJECT_ID', 'trans-century-292502'),
-//        'key_file' => storage_path('GG-services/trans-century-292502-f1667700ff23.json'), // optional: /path/to/service-account.json
-//        'bucket' => env('GOOGLE_CLOUD_STORAGE_BUCKET', 'library-media'),
-//        'path_prefix' => env('GOOGLE_CLOUD_STORAGE_PATH_PREFIX', null), // optional: /default/path/to/apply/in/bucket
-//        'storage_api_uri' => env('GOOGLE_CLOUD_STORAGE_API_URI', null), // see: Public URLs below
-//        'visibility' => 'public',
-//    ],
-//    'minio' => [
-//        'driver' => 'minio',
-//        'key' => env('MINIO_KEY', 'minio'),
-//        'secret' => env('MINIO_SECRET', 'minio123'),
-//        'region' => '',
-//        'bucket' => env('MINIO_BUCKET', 'media'),
-//        'endpoint' => env('MINIO_ENDPOINT', 'http://minio:9000'),
-//        'use_path_style_endpoint' => true,
-//    ],
-//];
+if (env('TRAVIS_TESTING') !== true) {
+    $disksConfig = [
+        'local' => [
+            'driver' => 'local',
+            'root' => storage_path('app/public'),
+            'url' => env('APP_URL') . '/storage/public',
+            'visibility' => 'public',
+        ],
+        's3' => [
+            'driver' => 's3',
+            'key' => env('AWS_ACCESS_KEY_ID'),
+            'secret' => env('AWS_SECRET_ACCESS_KEY'),
+            'region' => env('AWS_DEFAULT_REGION'),
+            'bucket' => env('AWS_BUCKET'),
+            'url' => env('AWS_URL'),
+        ],
+        'gcs' => [
+            'driver' => 'gcs',
+            'project_id' => env('GOOGLE_CLOUD_PROJECT_ID', 'trans-century-292502'),
+            'key_file' => storage_path('GG-services/trans-century-292502-f1667700ff23.json'), // optional: /path/to/service-account.json
+            'bucket' => env('GOOGLE_CLOUD_STORAGE_BUCKET', 'library-media'),
+            'path_prefix' => env('GOOGLE_CLOUD_STORAGE_PATH_PREFIX', null), // optional: /default/path/to/apply/in/bucket
+            'storage_api_uri' => env('GOOGLE_CLOUD_STORAGE_API_URI', null), // see: Public URLs below
+            'visibility' => 'public',
+        ],
+        'minio' => [
+            'driver' => 'minio',
+            'key' => env('MINIO_KEY', 'minio'),
+            'secret' => env('MINIO_SECRET', 'minio123'),
+            'region' => '',
+            'bucket' => env('MINIO_BUCKET', 'media'),
+            'endpoint' => env('MINIO_ENDPOINT', 'http://minio:9000'),
+            'use_path_style_endpoint' => true,
+        ],
+    ];
 
-$disksConfig = config('filesystems');
+    $configs = config('filesystems');
 
-//foreach ($listDisks as $disk) {
-//    if (array_key_exists($disk, $configs['disks'])) {
-//        unset($disksConfig[$disk]);
-//    }
-//}
+    foreach ($listDisks as $disk) {
+        if (array_key_exists($disk, $configs['disks'])) {
+            unset($disksConfig[$disk]);
+        }
+    }
+} else {
+
+    $disksConfig = config('filesystems');
+
+}
+
 
 return [
     'HTTP_STATUS_CODE' => [
